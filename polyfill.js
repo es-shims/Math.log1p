5fb7003867fe94c1f7fa0b45c35db7f746bf3a8c
Jordan Harband <ljharb@gmail.com>
2022-04-10 21:00:46 -0700

    [Fix] avoid identifier `native`; it breaks node 0.4

--- polyfill.js
+++ polyfill.js
@@ -3,12 +3,12 @@
 var implementation = require('./implementation');
 
 module.exports = function getPolyfill() {
-	var native = Math.log1p;
+	var orig = Math.log1p;
 
 	// IE 11 TP has an imprecise log1p: reports Math.log1p(-1e-17) as 0
-	if (!native || native(-1e-17) !== -1e-17) {
+	if (!orig || orig(-1e-17) !== -1e-17) {
 		return implementation;
 	}
 
-	return native;
+	return orig;
 };
