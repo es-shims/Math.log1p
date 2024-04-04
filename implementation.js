b5317cc32295f9c43a6f6f011cd05ee3c4664446
Jordan Harband <ljharb@gmail.com>
2024-04-04 14:46:28 -0500

    [Deps] update `call-bind`, `define-properties`, `es-abstract`, `get-intrinsic`

--- implementation.js
+++ implementation.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var ToNumber = require('es-abstract/2023/ToNumber');
+var ToNumber = require('es-abstract/2024/ToNumber');
 var numberIsNan = require('es-abstract/helpers/isNaN');
 
 var GetIntrinsic = require('get-intrinsic');
